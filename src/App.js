9cf7776c7b53fc35609838e9268a8cb56c4e8a9f
Ahsan Habib <ahsanpub16b@gmail.com>
2022-04-18 00:29:36 +0600

    added slider here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home/Home';
 import Header from './components/Sheared-Component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+
+      <Routes>
+        <Route path="/home" element={<Home></Home>} />
+      </Routes>
 
     </div>
   );
